628a15dd0cd4349b8b6e8e5a263512193fa1c6f5
dougy147 <16362303+dougy147@users.noreply.github.com>
2024-01-28 08:28:29 +0100

    Allow command line arguments.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-`olenia` is an OCaml implementation of [Lenia](https://en.wikipedia.org/wiki/Lenia), a non-discrete Game of Life.
+`olenia` is an OCaml implementation of a basic version of [Lenia](https://en.wikipedia.org/wiki/Lenia), a non-discrete Game of Life, focused on real-time visualization.
 
 # Quick launch
 
@@ -14,12 +14,30 @@ $ ./olenia
 | `i`      | Toggle informations                        |
 | `<Esc>`  | Close                                      |
 
-
 <p align="center">
 <img src="https://raw.githubusercontent.com/dougy147/olenia/master/sample.gif" width="75%" />
 </p>
 
 Despite using FFT, `olenia` is slow: 16fps on 64x64 maps, and only 2fps on 128x128.
-However, as its main feature is real-time visualization, it is important to improve execution speed.
+As its main feature is real-time visualization, improving execution speed is important.
 For now, it probably suffers from the excessive use of `Array.map` and `Array.init`. 
-Neither flattening the map to a single array, nor allocating memory for the arrays outside of the functions helps speeding things up.
+Neither flattening the map to a single array, nor initializing arrays outside of their functions (that get called a lot) helped speeding things up.
+Allowing real-time modification of parameters would also be nice.
+
+
+```sh
+$ ./olenia --help
+olenia [-s int] [-w int] [-h int] [-mu float] [-sd float] [-kmu float] [-ksd float] [-radius int] [-time float] [-scale int] [-shape <ellipse|square>] [-theme <borealis|redsoft|tournesol>]
+  -s : map size (int)
+  -w : window width (int)
+  -h : window height (int)
+  -time : time discretization (float)
+  -mu : convolution gaussian mean (float)
+  -sd : convolution gaussian standard deviation (float)
+  -kmu : kernel gaussian mean (float)
+  -ksd : kernel gaussian standard deviation (float)
+  -radius : kernel radius (int)
+  -scale : zoom level (int)
+  -shape : cells shape (ellipse or square)
+  -theme : color gradient (borealis, redsoft or tournesol)
+```
--- olenia.ml
+++ olenia.ml
@@ -1,27 +1,62 @@
 Random.self_init ()
 
-let global_size = 64
-
 let cell () = Random.float 1.
 (*let cell () = if Random.float 1. < 0.775 then 0. else cell ()*)
 
-let time  = 10.
-let scale = 1
-let kernel_radius = 13 * scale
-let map_size  = global_size
-let half_size = map_size / 2
+(* Parameters initialization (command-line arguments parsing if any) *)
+let usage_msg = "olenia [-s int] [-w int] [-h int] [-mu float] [-sd float] [-kmu float] [-ksd float] [-radius int] [-time float] [-scale int] [-shape <ellipse|square>] [-theme <borealis|redsoft|tournesol>]"
 
-let world = Array.init map_size (fun _ -> Array.init map_size (fun _ -> cell ()))
+let time          = ref 10.
+let scale         = ref 1
+let kernel_radius = ref 13
+let convo_mu      = ref 0.15
+let convo_sigma   = ref 0.015
+let kernel_mu     = ref 0.5
+let kernel_sigma  = ref 0.15
+let map_size      = ref 64
+let width         = ref 800
+let height        = ref 800
+let shape         = ref "ellipse"
+let theme         = ref "borealis"
+
+let speclist =
+  [
+    ("-s", Arg.Set_int map_size, ": map size (int)");
+    ("-w", Arg.Set_int width, ": window width (int)");
+    ("-h", Arg.Set_int height, ": window height (int)");
+    ("-time", Arg.Set_float time, ": time discretization (float)");
+    ("-mu", Arg.Set_float convo_mu, ": convolution gaussian mean (float)");
+    ("-sd", Arg.Set_float convo_sigma, ": convolution gaussian standard deviation (float)");
+    ("-kmu", Arg.Set_float kernel_mu, ": kernel gaussian mean (float)");
+    ("-ksd", Arg.Set_float kernel_sigma, ": kernel gaussian standard deviation (float)");
+    ("-radius", Arg.Set_int kernel_radius, ": kernel radius (int)");
+    ("-scale", Arg.Set_int scale, ": zoom level (int)");
+    ("-shape", Arg.Set_string shape, ": cells shape (ellipse or square)");
+    ("-theme", Arg.Set_string theme, ": color gradient (borealis, redsoft or tournesol)");
+  ]
+
+let parse_arguments = Arg.parse speclist (fun _ -> ()) usage_msg;;
+parse_arguments
+;;
+
+let draw_shape x y sw sh =
+  match !shape with
+  | "ellipse" -> Graphics.fill_ellipse (x * sw) (y * sh) (sw/2) (sh/2)
+  | "square"  -> Graphics.fill_rect (x * sw) (y * sh) (sw) (sh)
+  | _ -> failwith "This shape does not exist."
+
+let half_size = !map_size / 2
+(* End of parameters initialization *)
+
+let world: float array array = Array.init !map_size (fun _ -> Array.init !map_size (fun _ -> cell ()))
 
 let initialize_world () =
   Array.iteri (fun i r -> Array.iteri (fun j c -> world.(i).(j) <- cell () ) r) world
 
 let gaussian_function x mu sd: float = exp (-. ( ( (x -. mu) /. sd) ** 2.) /. 2.)
-let mu = 0.15
-let sigma = 0.015
 
-let build_distance (radius: int) =
-  let map_middle = Array.init (map_size) (fun x -> float_of_int (x - (map_size / 2) - (if map_size mod 2 = 0 then 1 else 2) ) ) in
+let build_distance (radius: int) = (* distance from the center of the kernel *)
+  let map_middle = Array.init (!map_size) (fun x -> float_of_int (x - (!map_size / 2) - (if !map_size mod 2 = 0 then 1 else 2) ) ) in
   let s = Array.length map_middle in
   let d = Array.make_matrix (s) (s) 0. in
   Array.iteri (fun i r -> Array.iteri (fun j c -> d.(i).(j) <- ( (sqrt( (map_middle.(i) ** 2.) +. (map_middle.(j) ** 2.))) /. (float_of_int radius) )  ) r) d;
@@ -30,18 +65,16 @@ let build_distance (radius: int) =
 let build_kernel (distance: float array array) =
   let s = Array.length distance in
   let k = Array.make_matrix s s  0. in
-  let kernel_mu = 0.5 in
-  let kernel_sigma = 0.15 in
   let normalize_kernel (kernel: float array array) =
     let sum = List.fold_left (fun acc x -> x +. acc ) 0. (Array.to_list (Array.map (fun x -> Array.to_list x) kernel) |> List.flatten) in
     Array.iteri (fun i r -> Array.iteri (fun j c -> kernel.(i).(j) <- (kernel.(i).(j) /. sum) ) r) kernel;
     kernel
   in
-  Array.iteri (fun i r -> Array.iteri (fun j c -> k.(i).(j) <- if distance.(i).(j) < 1. then (1. *. (gaussian_function distance.(i).(j) kernel_mu kernel_sigma)) else 0. ) r) distance;
+  Array.iteri (fun i r -> Array.iteri (fun j c -> k.(i).(j) <- if distance.(i).(j) < 1. then (1. *. (gaussian_function distance.(i).(j) !kernel_mu !kernel_sigma)) else 0. ) r) distance;
   normalize_kernel k
 
 let apply_growth (convolution: float array array) =
-  let f i j c = let ns = ( c +. ( ( 1. /. time ) *. ( ( (gaussian_function (convolution.(i).(j)) mu sigma) *. 2. ) -. 1.) ) ) in
+  let f i j c = let ns = ( c +. ( ( 1. /. !time ) *. ( ( (gaussian_function (convolution.(i).(j)) !convo_mu !convo_sigma) *. 2. ) -. 1.) ) ) in
     if ns < 0. then 0.
     else
       if ns > 1. then 1.
@@ -49,12 +82,18 @@ let apply_growth (convolution: float array array) =
     in
   Array.iteri (fun i r -> Array.iteri ( fun j c -> world.(i).(j) <- f i j c ) r) world
 
-(* Operations between floats and complex numbers *)
+(* Operations between floats and complex numbers or latter only *)
 let mult_float_complex (x: float) (c: Complex.t): Complex.t =
   Complex.{re=x*.c.re; im=x*.c.im}
 
 let div_complex_float (c: Complex.t) (x: float): Complex.t =
   Complex.{re=c.re/.x; im=c.im/.x}
+
+let complex_to_real_matrix (matrix: Complex.t array array) =
+  Array.map (fun row -> Array.map (fun Complex.{re=elem;im=_} -> elem) row) matrix
+
+let mult_complex_matrices (mat1: Complex.t array array) (mat2: Complex.t array array): Complex.t array array =
+  Array.map2 (fun r1 r2 -> Array.map2 (Complex.mul) r1 r2 ) mat2 mat1
 
 (* Fourier Transform related functions *)
 let fft_shift (matrix: 'a array array): 'a array array =
@@ -109,63 +148,66 @@ let rec ifft_1D (v: Complex.t array): Complex.t array =
   let f acc k n' = Complex.add (acc) (Complex.mul (v.(n')) (Complex.pow (w) Complex.{re=float_of_int (-k * n');im=0.})) in
   Array.map (fun k -> div_complex_float (Array.fold_left (fun acc n' -> f acc k n') (Complex.zero) (v_indexes)) (float_of_int n) ) v_indexes
 
-let fft2 (matrix: float array array) =
-  let fft_col = Array.init map_size (fun c -> fft_1D (Array.init map_size (fun r -> Complex.{re=matrix.(r).(c);im=0.}))) in
-  Array.init map_size (fun r -> fft_1D (Array.init map_size (fun c -> fft_col.(c).(r))))
+let fft_2D (matrix: float array array) =
+  let fft_col = Array.init !map_size (fun c -> fft_1D (Array.init !map_size (fun r -> Complex.{re=matrix.(r).(c);im=0.}))) in
+  Array.init !map_size (fun r -> fft_1D (Array.init !map_size (fun c -> fft_col.(c).(r))))
 
-let ifft2 matrix =
-  let fft_col = Array.init map_size (fun c -> ifft_1D (Array.init map_size (fun r -> matrix.(r).(c)))) in
-  Array.init map_size (fun r -> ifft_1D (Array.init map_size (fun c -> fft_col.(c).(r))))
+let ifft_2D (matrix: Complex.t array array) =
+  let ifft_col = Array.init !map_size (fun c -> ifft_1D (Array.init !map_size (fun r -> matrix.(r).(c)))) in
+  Array.init !map_size (fun r -> ifft_1D (Array.init !map_size (fun c -> ifft_col.(c).(r))))
 
-let complex_to_real_matrix (matrix: Complex.t array array) =
-  Array.map (fun row -> Array.map (fun Complex.{re=elem;im=_} -> elem) row) matrix
 
-let mult_complex_matrices (mat1: Complex.t array array) (mat2: Complex.t array array): Complex.t array array =
-  Array.map2 (fun r1 r2 -> Array.map2 (Complex.mul) r1 r2 ) mat2 mat1
+(* Construct kernel *)
+let k  = build_kernel (build_distance (!kernel_radius * !scale))
+let fk = fft_2D (fft_shift k)
 
-let k = build_kernel (build_distance kernel_radius)
-let fk = fft2 (fft_shift k)
-
-let next_world () =
-  fft2 world
+let next_world (world: float array array) =
+  world
+    |> fft_2D
     |> mult_complex_matrices fk
-    |> ifft2
+    |> ifft_2D
     |> complex_to_real_matrix
     |> apply_growth
 ;;
 
-let title  = "OLenia - Lenia in OCaml"
+
+let title  = "OLenia"
+(*
 let width  = 800
 let height = 800
-let grid   = global_size
-let scaled_width  = width  / grid ;;
-let scaled_height = height / grid ;;
-let cell_width  = width  / scaled_width ;;
-let cell_height = height / scaled_height ;;
-let size_string = " " ^ (string_of_int (width-cell_width)) ^ "x" ^ (string_of_int (height-cell_height));;
-
-type color = {r: float; g: float; b: float};;
-(* Theme Borealis *)
-let lower_color  = {r=20.; g=16.; b=110.};;
-let higher_color = {r=0.; g=255.; b=14.};;
-(* Theme RedSoft *)
-let lower_color  = {r=47.; g=72.; b=88.};;
-let higher_color = {r=217.; g=69.; b=100.};;
-(* Theme Tournesol *)
-(*
-let lower_color  = {r=47.; g=72.; b=88.};;
-let higher_color = {r=209.; g=217.; b=69.};;
 *)
+let grid   = !map_size
+let scaled_width  = !width  / grid
+let rest_width    = !width  - (grid * scaled_width)
+let cell_width    = !width  / scaled_width
+let scaled_height = !height / grid
+let rest_height   = !height - (grid * scaled_height)
+let cell_height   = !height / scaled_height
+let size_string   = " " ^ (string_of_int (scaled_width * cell_width - rest_width)) ^ "x" ^ (string_of_int (scaled_height * cell_height - rest_height))
 
+type color = {r: float; g: float; b: float}
+
+let lower_color  =
+  match !theme with
+  | "borealis"  -> {r=20.; g=16.; b=110.}
+  | "redsoft"   -> {r=47.; g=72.; b=88.}
+  | "tournesol" -> {r=47.; g=72.; b=88.}
+  | _ -> failwith "This theme does not exist";;
+
+let higher_color  =
+  match !theme with
+  | "borealis"  -> {r=0.; g=255.; b=14.}
+  | "redsoft"   -> {r=217.; g=69.; b=100.}
+  | "tournesol" -> {r=209.; g=217.; b=69.}
+  | _ -> failwith "This theme does not exist";;
+
+(* Open window *)
 Graphics.open_graph size_string;;
 Graphics.set_window_title title;;
 
-(*Set background color*)
 let set_background_color =
   Graphics.set_color (Graphics.rgb (int_of_float (lower_color.r)) (int_of_float (lower_color.g)) (int_of_float (lower_color.b)));
-  Graphics.fill_rect 0 0 width height;;
-
-(* let float_to_color f_num = int_of_float (f_num *. 255.) *)
+  Graphics.fill_rect 0 0 !width !height;;
 
 let float_to_color f_num =
   let red   = (int_of_float (lower_color.r +. (f_num *. ( higher_color.r -. lower_color.r )))) in
@@ -176,10 +218,8 @@ let float_to_color f_num =
 let draw_point x y color =
   let c = float_to_color color in (* convert value in cell to a color *)
   Graphics.set_color c;
-  Graphics.fill_ellipse ((x-1) * scaled_width) ((y-1) * scaled_height) (scaled_width/2) (scaled_height/2);;
-  (* If you prefer to draw squares instead of circles
-  Graphics.fill_rect ((x-1) * scaled_width) ((y-1) * scaled_height) scaled_width scaled_height;;
-  *)
+  draw_shape x y scaled_width scaled_height
+  (*Graphics.fill_ellipse (x * scaled_width) (y * scaled_height) (scaled_width/2) (scaled_height/2);;*)
 
 let print_world () =
   Array.iteri (fun i r -> Array.iteri (fun j c -> draw_point i j c) r) world
@@ -188,8 +228,8 @@ let display_info (generation: int) (time: float) =
   Graphics.moveto (1) (1);
   Graphics.fill_rect 0 0 (9 * scaled_width) (3 * scaled_height);
   let line1 = "G: " ^ string_of_int generation ^ " ; GPS=" ^ string_of_float (100. *. (1. /. time) |> Float.round |> (fun x -> x /. 100.) ) in
-  let line2 = "Map: " ^ string_of_int global_size ^ "x" ^ string_of_int global_size ^ " ; R=" ^ string_of_int kernel_radius in
-  let line3 = "Gauss: m=" ^ string_of_float mu ^ " s=" ^ string_of_float sigma in
+  let line2 = "Map: " ^ string_of_int grid ^ "x" ^ string_of_int grid ^ " ; R=" ^ string_of_int !kernel_radius in
+  let line3 = "Gauss: m=" ^ string_of_float !convo_mu ^ " sd=" ^ string_of_float !convo_sigma in
 
   Graphics.set_color Graphics.white;
   Graphics.draw_string line1;
@@ -211,10 +251,9 @@ let bigbang world =
       | _      -> ()
     else
       print_world ();
-      (*Printf.printf "%d" generation;*)
       if display then display_info generation (Sys.time() -. !t1) else ();
       t1 := Sys.time();
-      next_world ();
+      next_world world;
       aux ~display (generation+1) w
   in
   aux 0 world
